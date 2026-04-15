e66b43eb45c7bc8b0122cf3a9b81280130656a04
Angelica <lisoveckaya1997@gmail.com>
2026-04-15 12:00:51 +0300

    ю

--- src/api/liqpay/routes/liqpay.ts
+++ src/api/liqpay/routes/liqpay.ts
@@ -2,10 +2,18 @@ export default {
   routes: [
     {
       method: 'POST',
-      path: '/liqpay/create',
+      path: '/liqpay/create', // Для створення платежу (викликає фронтенд)
       handler: 'liqpay.create',
       config: {
         auth: false, 
+      },
+    },
+    {
+      method: 'POST',
+      path: '/liqpay/handle', // Для отримання статусу (викликає LiqPay)
+      handler: 'callback.handle', 
+      config: {
+        auth: false,
       },
     },
   ],

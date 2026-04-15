56ffc857c8c88b7e13f18ff12e9858025358b524
Angelica <lisoveckaya1997@gmail.com>
2026-04-15 11:33:31 +0300

    ю

--- src/api/liqpay/controllers/callback.ts
+++ src/api/liqpay/controllers/callback.ts
@@ -5,21 +5,27 @@ const privateKey = process.env.LIQPAY_PRIVATE_KEY!;
 
 export default {
   async handle(ctx) {
+    console.log("--- ВХІДНИЙ ЗАПИТ ВІД LIQPAY ---");
     const { data, signature } = ctx.request.body;
+    console.log("Отримані дані (base64):", data);
 
     const expectedSignature = CryptoJS.SHA1(privateKey + data + privateKey)
       .toString(CryptoJS.enc.Base64);
 
     if (signature !== expectedSignature) {
+      console.log("❌ ПОМИЛКА: Підписи не збігаються!");
       return ctx.badRequest('Invalid signature');
     }
 
     const decoded = JSON.parse(
       Buffer.from(data, 'base64').toString('utf-8')
     );
+    console.log("Статус платежу від LiqPay:", decoded.status);
 
-   if(decoded.status === 'success' || decoded.status === 'sandbox'){
+   if (decoded.status === 'success' || decoded.status === 'sandbox') {
 const orderNumber = String(decoded.order_id);
+console.log("Шукаємо замовлення номер:", decoded.order_id);
+
 
   const orders = await strapi.entityService.findMany('api::order.order', {
     filters: {
